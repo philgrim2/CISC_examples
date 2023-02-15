ccac27a68cbf6c1f4721842bfb03fe856173db3b
Phil Grim <phil@insufficient-light.com>
2023-02-15 00:01:22 -0500

    Added example login app.

---
+++ CISC349/LoginExample/app/src/main/java/edu/harrisburgu/cisc349/loginexample/LoginFragment.java
@@ -0,0 +1,24 @@
+package edu.harrisburgu.cisc349.loginexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.layout_login, container, false);
+        return v;
+    }
+
+}
---
+++ CISC349/LoginExample/app/src/main/java/edu/harrisburgu/cisc349/loginexample/MainActivity.java
@@ -0,0 +1,27 @@
+package edu.harrisburgu.cisc349.loginexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new LoginFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
