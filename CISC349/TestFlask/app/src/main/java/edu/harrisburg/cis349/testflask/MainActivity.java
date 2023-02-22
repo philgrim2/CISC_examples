d669bf11c3b5cf93a06474734fc41e3af1718a3b
Phil Grim <phil@insufficient-light.com>
2023-02-22 15:44:09 -0500

    Adding data file.

---
+++ CISC349/TestFlask/app/src/main/java/edu/harrisburg/cis349/testflask/MainActivity.java
@@ -0,0 +1,46 @@
+package edu.harrisburg.cis349.testflask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+
+        String url = "http://10.1.120.71:5000/";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.start();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
